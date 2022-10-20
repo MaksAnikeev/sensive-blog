04df67666e0e9d9b9dfbc530ab448be1e50cbe37
MaksAnikeev <anikeev.maks@rambler.ru>
2022-10-20 19:56:00 +0300

    Show popular posts on main page

--- blog/views.py
+++ blog/views.py
@@ -27,9 +27,17 @@ def serialize_tag(tag):
     }
 
 
-def index(request):
+def get_likes_count(post):
+    return post.likes.count()
 
-    most_popular_posts = []  # TODO. Как это посчитать?
+def index(request):
+    posts = Post.objects.all()
+    sorted_post = sorted(posts, key=get_likes_count, reverse=True)
+
+    most_popular_posts = sorted_post[:5]
+    for post in most_popular_posts:
+        print(post.likes.count())
+
 
     fresh_posts = Post.objects.order_by('published_at')
     most_fresh_posts = list(fresh_posts)[-5:]
